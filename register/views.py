de34ec4c87c8321492003c5f3cae61805f69f9df
Theodoreshub <shijialonga@gmail.com>
2020-12-17 11:49:00 +0800

    check form in front end and rear end. Then will use form feature provided by Djangp

--- register/views.py
+++ register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
+from . import models
 
 # Create your views here.
 def index(request):
@@ -8,7 +9,25 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容！'
+        if username.strip() and password:
+            #这里可以填入一些验证
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在！'
+                return render(request, 'register/login.html', {'message': message})
+            if user.password == password:
+                print(username, password)
+                return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'register/login.html', {'message': message})
+        else:
+            return render(request, 'register/login.html', {'message': message})
     return render(request, 'register/login.html')
 
 
